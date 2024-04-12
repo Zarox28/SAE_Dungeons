6334b029aaa02eced1f61ac1341939bcc5ed9e4e
0x454d505459 <testerthe60@gmail.com>
2024-04-12 13:43:58 +0200

    🔄 Slighly better graph generation and scrambling

--- include/dungeon/dungeon.h
+++ include/dungeon/dungeon.h
@@ -9,12 +9,14 @@
 class Dungeon
 {
     private:
-  unsigned        hallway_length = 3;
-  Graph           graph;
+  unsigned hallway_length = 3;
+  unsigned start_node;
+  Graph    graph;
+
   inline unsigned CalculateGridSize();
 
     public:
-  Dungeon(unsigned width, unsigned height) noexcept(false);
+  Dungeon(unsigned width, unsigned height, unsigned start_x, unsigned start_y) noexcept(false);
   void        set_hallway_length(unsigned length) noexcept(false);
   unsigned    get_hallway_length() noexcept;
   std::string RenderAsciiFrame();
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "src/dungeon/dungeon.cpp"
+
 #include <iostream>
 
 int main()
 {
-  Dungeon* test_dungeon = new Dungeon(6, 6);
+  Dungeon* test_dungeon = new Dungeon(6, 6, 0, 0);
   std::cout << test_dungeon->RenderAsciiFrame();
 }
